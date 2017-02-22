c59770cd5a7b404a54c04f860712de1487445337
Max Rolon <max.rolon@barrelny.com>
2017-02-22 14:09:07 -0500

    Adds (Hacky) support for Shopify liquid tag
    Supports a hardcoded Shopify CDN if NODE_ENV is not 'production'. Use this if the css file is coming from a webpack dev server.

--- index.js
+++ index.js
@@ -7,22 +7,44 @@ var postcss = require('postcss');
 var url = require('url');
 
 module.exports = postcss.plugin('postcss-fontpath', function (opts) {
+  console.dir( opts )
+
   opts = objectAssign({
-    checkPath: false
+    checkPath: false,
+    shopifyCDNPath: '',
   }, opts);
 
   return function (css, result) {
-
     // Loop through each @rule
     css.walkAtRules('font-face', function(rule) {
 
       // Loop through each decleration in the rule
       rule.walkDecls('font-path', function(decl) {
 
+        let url = decl.value.replace(/"/g, '')
+
+        const isShopify = /^{{.*}}$/.test( url )
+
+        const isProd = process.env.NODE_ENV === 'production'
+
+        const addPrefix = ( str, ext ) => {
+          if ( isShopify ) {
+            let mutated
+            if ( isProd ) {
+              mutated = str.replace(/(\{\{ ?'.[^']*)/,  '$1' + ext )
+            } else {
+              mutated = opts.shopifyCDNPath + str.match(/\{\{ ?'(.[^']*)/)[1] + ext
+            }
+            return mutated
+          } else {
+            return `${str}.${ext}`
+          }
+        }
+
         // Gather up the components of our new declerations
-        var fontPath = decl.value.replace(/'/g, ''),
+        var fontPath = decl.value.replace(/"/g, ''),
             src = '',
-            ieSrc = 'url("' + fontPath + '.eot")',
+            ieSrc = 'url("' + addPrefix( url, 'eot' ) + '")',
             formats = [
               { type: 'embedded-opentype', ext: '.eot?#iefix' },
               { type: 'woff', ext: '.woff' },
@@ -34,6 +56,7 @@ module.exports = postcss.plugin('postcss-fontpath', function (opts) {
         formats.forEach(function(format, index, array) {
 
           if (opts.checkPath === true) {
+
             // Make the fontPath absolute and normalize it (removes the #iefix hash)
             var absoluteFontPath = url.parse(path.resolve(path.dirname(css.source.input.file), fontPath) + format.ext).pathname;
 
@@ -48,9 +71,9 @@ module.exports = postcss.plugin('postcss-fontpath', function (opts) {
           }
 
           if (index === array.length - 1){
-            src += 'url("' + fontPath + format.ext + '") format(\'' + format.type + '\')';
+            src += 'url("' + addPrefix( url, format.ext ) + '") format(\'' + format.type + '\')';
           } else {
-            src += 'url("' + fontPath + format.ext + '") format(\'' + format.type + '\'),\n       ';
+            src += 'url("' + addPrefix( url, format.ext ) + '") format(\'' + format.type + '\'),\n       ';
           }
 
         });
